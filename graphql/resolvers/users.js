2d9755c988ca6f301f99376c81ef8e354b757575
Isaac <iavitia@outlook.com>
2020-01-17 19:26:50 -0700

    Implement log in & validate register input

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,22 +1,91 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const { SECRET_KEY } = require('../../config');
+const {
+  validateRegisterInput,
+  validateLoginInput
+} = require('../../utils/validators');
 const User = require('../../models/User');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError(
+          'There isn’t an account associated with this username',
+          { errors }
+        );
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong credentials';
+        throw new UserInputError('The password you entered is incorrect', {
+          errors
+        });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      };
+    },
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // TODO: validate user data
+      // validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
       // TODO: make sure user doesn't already exist
-      // TODO: hash password and create auth token
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'Username already exists'
+          }
+        });
+      }
+
+      // hash password
       password = await bcrypt.hash(password, 12);
 
+      // create new user
       const newUser = new User({
         email,
         username,
@@ -26,15 +95,8 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username
-        },
-        SECRET_KEY,
-        { expiresIn: '1h' }
-      );
+      // create auth token
+      const token = generateToken(res);
 
       return {
         ...res._doc,
---
+++ utils/validators.js
@@ -0,0 +1,56 @@
+module.exports.validateRegisterInput = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (username.trim() === '') {
+    errors.username = 'Username is required ';
+  }
+  if (!username.match(/^[0-9A-Za-z\-]+$/)) {
+    errors.username =
+      'Username must contain letters, numbers, dashes, and underscores only';
+    // }
+  }
+  if (email.trim() === '') {
+    errors.email = 'Email is required ';
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+
+    if (!email.match(regEx)) {
+      errors.email = 'Email must be a valiid email address';
+    }
+  }
+  if (!password.match(/.{6,}/)) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+  if (password === '') {
+    errors.password = 'Password is required';
+  }
+  if (password !== confirmPassword) {
+    errors.confirmPassword = 'Passwords must match';
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+
+  if (username.trim() === '') {
+    errors.username = 'Username is required ';
+  }
+
+  if (password.trim() === '') {
+    errors.password = 'Password is required ';
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
