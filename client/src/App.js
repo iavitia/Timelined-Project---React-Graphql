7afb53fcd4361fc8d8e2dd24e143d3508ccd9e35
Isaac <iavitia@outlook.com>
2020-02-01 19:48:36 -0700

    simple routes & navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import { Home, Login, Register } from './components/pages';
+import { Navbar } from './components/organisms';
 
 function App() {
   return (
-    <div>
-      <h1>Start!</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
   );
 }
 
---
+++ client/src/components/organisms/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Image, Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+export default function() {
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
+
+  const [activeItem, setActiveItem] = useState(path);
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <Menu style={{ border: 'none' }} borderless={true} size='massive'>
+      <Menu.Item
+        as={Link}
+        to='/'
+        header
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+      >
+        <Image
+          style={{ height: '35px' }}
+          src='https://react.semantic-ui.com/logo.png'
+        />
+      </Menu.Item>
+
+      <Menu.Menu position='right'>
+        <Menu.Item
+          name='login'
+          as={Link}
+          to='/login'
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+        />
+
+        <Menu.Item
+          name='signup'
+          as={Link}
+          to='/register'
+          active={activeItem === 'signup'}
+          onClick={handleItemClick}
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
