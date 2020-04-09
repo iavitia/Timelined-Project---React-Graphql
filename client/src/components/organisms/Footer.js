d92358068a652e66fa02c1e92ecdeb201d5d7669
Isaac <iavitia@outlook.com>
2020-04-08 19:27:29 -0700

    new footer

--- client/src/components/organisms/Footer.js
+++ client/src/components/organisms/Footer.js
@@ -1,39 +1,62 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { Container, Image, List, Segment } from 'semantic-ui-react';
+import { Image, List, Segment, Divider, Grid } from 'semantic-ui-react';
 
-export default function() {
+export default function () {
   return (
     <Segment
-      inverted
       vertical
       style={{
-        margin: '5em 0em 0em',
-        padding: '2em 0em'
+        margin: '5em 3em 0em',
+        padding: '2em 0em',
       }}
     >
-      <Container textAlign='center'>
-        <Image
-          centered
-          size='mini'
-          src='https://react.semantic-ui.com/logo.png'
-        />
-        <List horizontal inverted divided link size='small'>
-          <List.Item as={Link} to='/'>
-            Home
-          </List.Item>
-          <List.Item as={Link} to='/contact'>
-            Contact Us
-          </List.Item>
-          <List.Item as={Link} to='terms'>
-            Terms and Conditions
-          </List.Item>
-          <List.Item as={Link} to='/privacy'>
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
+      <Grid centered>
+        <Grid.Column floated='left' width={2}>
+          <Image size='mini' src='/t3.svg' />
+        </Grid.Column>
+      </Grid>
+
+      <Divider />
+
+      <Grid>
+        <Grid.Column floated='left' width={5}>
+          <List horizontal link size='small'>
+            <List.Item>© 2020 Timlined</List.Item>
+          </List>
+        </Grid.Column>
+        <Grid.Column floated='right' width={11} textAlign='right'>
+          <List horizontal divided link size='small'>
+            <List.Item as={Link} to='/'>
+              Home
+            </List.Item>
+            <List.Item as={Link} to='/contact'>
+              Contact Us
+            </List.Item>
+            <List.Item as={Link} to='/terms'>
+              Terms and Conditions
+            </List.Item>
+            <List.Item as={Link} to='/privacy'>
+              Privacy Policy
+            </List.Item>
+          </List>
+        </Grid.Column>
+        {/* Social Media Links */}
+        {/* <Grid.Column floated='right' width={5} textAlign='right'>
+          <List horizontal link size='small'>
+            <List.Item as={Link} to='/'>
+              <List.Icon name='twitter' />
+            </List.Item>
+            <List.Item as={Link} to='/'>
+              <List.Icon name='instagram' />
+            </List.Item>
+            <List.Item as={Link} to='/'>
+              <List.Icon name='facebook' />
+            </List.Item>
+          </List>
+        </Grid.Column> */}
+      </Grid>
     </Segment>
   );
 }
